9b5a6816276304d26bab8cba3e7dd2206bcc3b8a
David Cuthbert <dacut@users.sourceforge.net>
2009-06-19 22:34:15 -0700

    Initial submission of header file.

---
+++ src/ublkdev.h
@@ -0,0 +1,19 @@
+/** @file   ublkdev.h
+ *  @brief  
+ */
+
+#ifndef UBLKDEV_H
+#define UBLKDEV_H
+
+#define UBLKDEV_KERNEL_VERSION          1
+#define UBLKDEV_KERNEL_MINOR_VERSION    0
+#define UBLKDEV_MODULE_VERSION          "1.0"
+
+#endif /* UBLKDEV_H */
+
+/* Local variables: */
+/* mode: C */
+/* indent-tabs-mode: nil */
+/* tab-width: 8 */
+/* End: */
+/* vi: set expandtab tabstop=8 */
